d07acafdc44b500ad640320bcb9a2b83683e6209
ITSTEP\st <st>
2024-05-23 18:33:58 +0300

    Use SOLID prinsiples: OCP and DIP

--- UniversityOOPExampleProject/GasStation.h
+++ UniversityOOPExampleProject/GasStation.h
@@ -1,33 +1,13 @@
 #pragma once
+#include "Transport.h"
 #include <iostream>
-#include "Car.h"
-#include "Bus.h"
-#include "Truck.h"
 
 using namespace std;
+
 
 class GasStation
 {
 public:
-	int calcTotalGas(Car* cars, Bus* buses, Truck* trucks,
-		int csize, int bsize, int tsize) {
-		
-		int total = 0;
-
-		for (int i = 0; i < csize; i++)
-		{
-			total += cars[i].volume;
-		}
-		for (int i = 0; i < bsize; i++)
-		{
-			total += buses[i].tank;
-		}
-		for (int i = 0; i < tsize; i++)
-		{
-			total += trucks[i].tank;
-		}
-
-		return total;
-	}
+	int calcTotalGas(Transport* transports, int size);
 };
 
--- UniversityOOPExampleProject/main.cpp
+++ UniversityOOPExampleProject/main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include "GasStation.h"
+#include "Transport.h"
+#include "Bus.h"
+#include "Car.h"
+#include "Truck.h"
+#include "Plane.h"
 
 using namespace std;
 
 int main() {
 
+	Car car1{ 45 }, car2{ 50 }, car3{ 55 };
+	Bus bus1{ 350 };
+	Truck truck1{ 550 }, truck2{ 450 };
+	Plane plane1{ 1500 };
 
+	Transport transports[] = { car1, car2, car3, bus1, truck1, truck2, plane1 };
+
+	GasStation station;
+
+	int total = station.calcTotalGas(transports, 7);
+
+	cout << "Total fuel is " << total << ".\n";
 
 	return 0;
 }
